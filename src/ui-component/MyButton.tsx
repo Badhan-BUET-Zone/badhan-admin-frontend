ed072c6664deb2eed5e0952819870b2d307c6749
Mir Mahathir Mohammad <36355905+mirmahathir1@users.noreply.github.com>
2022-07-14 00:46:09 +0600

    formik experiment on version page

--- src/ui-component/MyButton.tsx
+++ src/ui-component/MyButton.tsx
@@ -2,7 +2,7 @@ import {Button} from "@mui/material";
 import React from "react";
 import {CircularProgress} from "@mui/material";
 import styles from './MyButton.module.css'
-const MyButton: React.FC<{text: string, color: 'primary'|'warning'|'error', onClick: (()=> void), loading?: boolean, disabled?:boolean}> = (props) => (
+const MyButton: React.FC<{text: string, color?: 'primary'|'warning'|'error', onClick?: (()=> void), loading?: boolean, disabled?:boolean}> = (props) => (
     <Button className={styles.button} variant="outlined" color={props.color} onClick={props.onClick} disabled={props.loading || props.disabled}>
         {props.loading && <CircularProgress/>}
         {props.text}
--- src/views/management/Version.tsx
+++ src/views/management/Version.tsx
@@ -10,8 +10,68 @@ import {useDispatch} from "react-redux";
 import {NotificationError, NotificationSuccess} from "../../store/notificationModel";
 import MySkeleton from "../../ui-component/MySkeleton";
 import FadeAnimationWrapper from "../../ui-component/motion/FadeAnimationWrapper";
-import {Box} from "@mui/material";
+import {Box, Button, TextField} from "@mui/material";
 import styleClasses from './Version.module.css'
+
+import * as yup from 'yup';
+import {useFormik} from "formik";
+
+const validationSchema = yup.object({
+    email: yup
+        .string()
+        .email('Enter a valid email')
+        .required('Email is required'),
+    password: yup
+        .string()
+        .min(8, 'Password should be of minimum 8 characters length')
+        .required('Password is required'),
+});
+
+const WithMaterialUI = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: 'foobar@example.com',
+            password: 'foobar',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values: any) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    return (
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <TextField
+                    fullWidth
+                    id="email"
+                    name="email"
+                    label="Email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                />
+                <TextField
+                    fullWidth
+                    id="password"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={formik.touched.password && Boolean(formik.errors.password)}
+                    helperText={formik.touched.password && formik.errors.password}
+                />
+                <Button color="primary" variant="contained" fullWidth type="submit">
+                    Submit
+                </Button>
+                <MyButton onClick={()=>{}} text={'Submit'}/>
+            </form>
+        </div>
+    );
+};
+
 const Version: React.FC = () => {
     // STATE MANAGEMENT
     const [majorVersion, setMajorVersion] = useState<string>('')
@@ -101,6 +161,7 @@ const Version: React.FC = () => {
 
     // MAIN RENDERING
     return (<MyMainCard title="Set the App Version Deployed on Google Play">
+        <WithMaterialUI/>
         {versionPageContent}
     </MyMainCard>)
 };
